f8bc80486fb6560d673567bf0c5e5942430c092a
AzureAwakener <laurencepauldabarquez@iskolarngbayan.pup.edu.ph>
2026-01-10 22:27:12 +0800

    Implement Ohm's Law and Gravitational Force calculators

--- 02.cpp
+++ 02.cpp
@@ -1,8 +1,67 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   // Kinematic Equations for Motion
+class OhmsLaw {
+public:
+    int getVoltage(int i, int r) {
+        return i * r;
+    }
 
-   return 0;
+    int getCurrent(int v, int r) {
+        return v / r;
+    }
+
+    int getResistance(int v, int i) {
+        return v / i;
+    }
+};
+
+int main() {
+    int v = 0; // Voltage
+    int i = 0; // Current
+    int r = 0; // Resistance
+    int choice;
+
+    OhmsLaw calc;
+
+    cout << "Welcome to Ohm's Law Calculator!";
+    cout << "\n\nPick an Operation:";
+    cout << "\n1 Voltage";
+    cout << "\n2 Current";
+    cout << "\n3 Resistance";
+    cout << "\nEnter Operation Number: ";
+    cin >> choice;
+
+    switch (choice) {
+    case 1:
+        cout << "Enter the value of Current: ";
+        cin >> i;
+        cout << "Enter the value of Resistance: ";
+        cin >> r;
+        cout << i << " * " << r << " = " << calc.getVoltage(i, r) << "V";
+        break;
+
+    case 2:
+        cout << "Enter the value of Voltage: ";
+        cin >> v;
+        cout << "Enter the value of Resistance: ";
+        cin >> r;
+        cout << v << " / " << r << " = " << calc.getCurrent(v, r) << "A";
+
+        break;
+
+    case 3:
+        cout << "Enter the value of Voltage: ";
+        cin >> v;
+        cout << "Enter the value of Current: ";
+        cin >> i;
+        cout << v << " / " << i << " = " << calc.getResistance(v, i) << "Ω";
+        
+        break;
+
+    default:
+        cout << "Invalid Operation";
+    }
+
+    return 0;
 }
--- 06.cpp
+++ 06.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    // Gravitational Force
+class GravitationalForceCalculator {
+private:
+    const double g = 6.67e-11; // Gravitational constant
 
-   return 0;
+public:
+    // Method to calculate gravitational force
+    double calculateForce(int mass1, int mass2, int distance) {
+        return (g * mass1 * mass2) / (distance * distance);
+    }
+};
+
+int main() {
+    GravitationalForceCalculator calculator;
+
+    cout << "Calculate Gravitational Force\n";
+    int mass1, mass2, distance;
+    cout << "Enter Mass 1: ";
+    cin >> mass1;
+    cout << "Enter Mass 2: ";
+    cin >> mass2;
+    cout << "Enter Distance: ";
+    cin >> distance;
+
+    double force = calculator.calculateForce(mass1, mass2, distance);
+    cout << "Gravitational Force: " << force << " N" << endl;
+
+    return 0;
 }
