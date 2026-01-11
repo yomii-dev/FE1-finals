a46086d9d86ed4ea4d57600b6a8475813c243c8d
yomiikawa <gabgonzaga28@gmail.com>
2026-01-11 18:43:52 +0800

    Made some stuff not be case-sensitive(Temperature Converter)

--- 07.cpp
+++ 07.cpp
@@ -1,3 +1,4 @@
+#include <cctype>
 #include <iostream>
 using namespace std;
 
@@ -17,8 +18,8 @@ class Temperature {
             
             cout << "\nDo you wish convert more? [y/n] ";
             cin >> repeat;
-            
-            if (repeat == 'n') {
+
+            if (repeat == 'n' || repeat == 'N') {
                 return;
             }
         }
@@ -46,6 +47,7 @@ class Temperature {
                 continue;
             }
 
+            unit = toupper(unit);
             return unit;
         }
     }
@@ -62,14 +64,15 @@ class Temperature {
                 continue;
             }
 
+            unit = toupper(unit);
             return unit;
         }
     }
 
     bool _isValidUnit(char input) {
-        char validUnits[3] = {'F', 'C', 'K'};
+        char validUnits[6] = {'F', 'C', 'K', 'f', 'c', 'k'};
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 6; i++) {
             if (input == validUnits[i]) {
                 return true;
             }
@@ -79,23 +82,30 @@ class Temperature {
     }
 
     double _solve(double temp, char initialUnit, char convertedUnit) {
+        bool fahrenheitToCelsius = (initialUnit == 'F' && convertedUnit == 'C');
+        bool fahrenheitToKelvin = (initialUnit == 'F' && convertedUnit == 'K');
+        bool celsiusToFahrenheit = (initialUnit == 'C' && convertedUnit == 'F');
+        bool celsiusToKelvin = (initialUnit == 'C' && convertedUnit == 'K');
+        bool kelvinToFahrenheit = (initialUnit == 'K' && convertedUnit == 'F');
+        bool kelvinToCelsius = (initialUnit == 'K' && convertedUnit == 'C');
+
         // if they, for some reason, picked the same unit to convert to
         if (initialUnit == convertedUnit) {
             cout << "You do realize that you converted it to the same unit "
                     "right?\n";
             return temp;
-        } else if (initialUnit == 'F' && convertedUnit == 'C') {
-            return ((temp - 32) / 1.8); // F -> C
-        } else if (initialUnit == 'F' && convertedUnit == 'K') {
-            return ((temp - 32) / 1.8) + 273.15; // F -> K
-        } else if (initialUnit == 'C' && convertedUnit == 'F') {
-            return (temp * 1.8) + 32; // C -> F
-        } else if (initialUnit == 'C' && convertedUnit == 'K') {
-            return temp + 273.15; // C -> K
-        } else if (initialUnit == 'K' && convertedUnit == 'F') {
-            return ((temp - 273.15) * 1.8) + 32; // K -> F
-        } else if (initialUnit == 'K' && convertedUnit == 'C') {
-            return temp - 273.15; // K -> C
+        } else if (fahrenheitToCelsius) {
+            return ((temp - 32) / 1.8);             // F -> C
+        } else if (fahrenheitToKelvin) {
+            return ((temp - 32) / 1.8) + 273.15;    // F -> K
+        } else if (celsiusToFahrenheit) {
+            return (temp * 1.8) + 32;               // C -> F
+        } else if (celsiusToKelvin) {
+            return temp + 273.15;                   // C -> K
+        } else if (kelvinToFahrenheit) {
+            return ((temp - 273.15) * 1.8) + 32;    // K -> F
+        } else if (kelvinToCelsius) {
+            return temp - 273.15;                   // K -> C
         } else {
             cout << "How tf did you get here???\n";
             return -1;
