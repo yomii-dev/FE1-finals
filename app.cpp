91b0f32bec72b5a3c3f8d13c2a133731837e66cf
yomii-dev <gabgonzaga28@gmail.com>
2026-01-12 23:32:39 +0800

    seven...

--- app.cpp
+++ app.cpp
@@ -462,7 +462,7 @@ class GravitationalForceCalculator {
         return (g * mass1 * mass2) / (distance * distance);
     }
 
-    public:
+  public:
     void run() {
         cout << "Calculate Gravitational Force\n";
         int mass1, mass2, distance;
@@ -478,6 +478,143 @@ class GravitationalForceCalculator {
     }
 };
 
+class Temperature {
+  public:
+    void run() {
+        char repeat;
+        cout << "=== Temperature Conversion Calculator :D ===\n";
+        while (true) {
+            double tempValue = _takeValue();
+            char initialUnit = _takeInitialUnit();
+            char convertedUnit = _takeConvertedUnit();
+
+            double answer = _solve(tempValue, initialUnit, convertedUnit);
+
+            cout << tempValue << initialUnit << " -> " << answer
+                 << convertedUnit;
+
+            cout << "\nDo you wish convert more? [y/n] ";
+            cin >> repeat;
+
+            if (repeat == 'n' || repeat == 'N') {
+                return;
+            }
+        }
+    }
+
+    /* Underscore naming conversion para di accidentally used */
+  private:
+    double _takeValue() {
+        while (true) {
+            double temp;
+
+            cout << "1. Input the VALUE of the temperature: ";
+
+            if (!(cin >> temp)) {
+                cin.clear();
+                cin.ignore(10000, '\n');
+                cout << "Please enter only real numbers\n";
+                continue;
+            }
+
+            return temp;
+        }
+    }
+
+    char _takeInitialUnit() {
+        char unit;
+
+        while (true) {
+            cout << "2. Pick the unit that your temperature IS IN [F/C/K]: ";
+            if (!(cin >> unit)) {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Please enter a valid input...\n";
+                continue;
+            }
+
+            /* This basically leaves only the first char */
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            if (!_isValidUnit(unit)) {
+                cout << "Please input a valid unit [F/C/K].\n";
+                continue;
+            }
+
+            unit = toupper(unit);
+            return unit;
+        }
+    }
+
+    char _takeConvertedUnit() {
+        char unit;
+
+        while (true) {
+            cout << "3. Pick the unit that you want to convert TO [F/C/K]: ";
+            if (!(cin >> unit)) {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Please enter a valid input...\n";
+                continue;
+            }
+
+            /* This basically leaves only the first char */
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            if (!_isValidUnit(unit)) {
+                cout << "Please input a valid unit [F/C/K].\n";
+                continue;
+            }
+
+            unit = toupper(unit);
+            return unit;
+        }
+    }
+
+    bool _isValidUnit(char input) {
+        char validUnits[6] = {'F', 'C', 'K', 'f', 'c', 'k'};
+
+        for (int i = 0; i < 6; i++) {
+            if (input == validUnits[i]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    double _solve(double temp, char initialUnit, char convertedUnit) {
+        bool fahrenheitToCelsius = (initialUnit == 'F' && convertedUnit == 'C');
+        bool fahrenheitToKelvin = (initialUnit == 'F' && convertedUnit == 'K');
+        bool celsiusToFahrenheit = (initialUnit == 'C' && convertedUnit == 'F');
+        bool celsiusToKelvin = (initialUnit == 'C' && convertedUnit == 'K');
+        bool kelvinToFahrenheit = (initialUnit == 'K' && convertedUnit == 'F');
+        bool kelvinToCelsius = (initialUnit == 'K' && convertedUnit == 'C');
+
+        // if they, for some reason, picked the same unit to convert to
+        if (initialUnit == convertedUnit) {
+            cout << "You do realize that you converted it to the same unit "
+                    "right?\n";
+            return temp;
+        } else if (fahrenheitToCelsius) {
+            return ((temp - 32) / 1.8); // F -> C
+        } else if (fahrenheitToKelvin) {
+            return ((temp - 32) / 1.8) + 273.15; // F -> K
+        } else if (celsiusToFahrenheit) {
+            return (temp * 1.8) + 32; // C -> F
+        } else if (celsiusToKelvin) {
+            return temp + 273.15; // C -> K
+        } else if (kelvinToFahrenheit) {
+            return ((temp - 273.15) * 1.8) + 32; // K -> F
+        } else if (kelvinToCelsius) {
+            return temp - 273.15; // K -> C
+        } else {
+            cout << "How tf did you get here???\n";
+            return -1;
+        }
+    }
+};
+
 class Menu {
   private:
     /* Class Initializations dito */
@@ -487,6 +624,7 @@ class Menu {
     Momentum fore;
     ProjectileMotion payb;
     GravitationalForceCalculator six;
+    Temperature seben;
     bool running = true;
     int choice;
 
@@ -536,7 +674,7 @@ class Menu {
                 break;
             case 7:
                 // Call ung calc nung choice
-
+                seben.run();
                 break;
             case 8:
                 // Call ung calc nung choice
