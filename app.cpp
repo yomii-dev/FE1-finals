2e289d199cd4f9076e79345e0307d11db4e53205
yomii-dev <gabgonzaga28@gmail.com>
2026-01-12 23:07:31 +0800

    3rd wan

--- app.cpp
+++ app.cpp
@@ -213,11 +213,88 @@ class OhmsLaw {
     }
 };
 
+class EnergyCalculations {
+  public:
+    void run() {
+        int choice;
+        const double g = 9.81;
+        double m;
+        double h;
+        double v;
+        double answer;
+
+        cout << "=== Energy Calculations Calculator :D ===\n";
+
+        while (true) {
+            cout << "Which are you looking for?\n";
+            cout << "1. Potential Energy(PE)\n2. Kinetic Energy(KE)\n3. Quit\n";
+            cout << "Choice: [1-3] ";
+            cin >> choice;
+
+            switch (choice) {
+            case 1:
+                m = getInput('m');
+                h = getInput('h');
+                answer = m * g * h;
+                cout << "PE = ";
+                break;
+            case 2:
+                m = getInput('m');
+                v = getInput('v');
+                answer = (m * v * v) / 2;
+                cout << "KE = ";
+                break;
+            case 3:
+                return;
+            default:
+                cout << "\nInvalid input\n\n";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                continue;
+            }
+
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            
+            cout << answer << "J\n";
+            return;
+        }
+    }
+
+    double getInput(char value) {
+        double input;
+
+        while (true) {
+            if (value == 'm') {
+                cout << "Enter the mass of the object in kilograms(kg): ";
+            } else if (value == 'v') {
+                cout << "Enter the velocity of the object in "
+                        "meters/second(m/s): ";
+            } else if (value == 'h') {
+                cout << "Enter the object's height above a reference point in "
+                        "meters(m): ";
+            } else {
+                cout << "uh what";
+            }
+            
+            if (!(cin >> input)) {
+                cout << "Please enter a number\n";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                continue;
+            }
+            
+            return input;
+        }
+
+    }
+};
+
 class Menu {
   private:
     /* Class Initializations dito */
     UnitConversion wan;
     OhmsLaw too;
+    EnergyCalculations tree;
     bool running = true;
     int choice;
 
@@ -251,7 +328,7 @@ class Menu {
                 break;
             case 3:
                 // Call ung calc nung choice
-
+                tree.run();
                 break;
             case 4:
                 // Call ung calc nung choice
